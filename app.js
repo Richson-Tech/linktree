7772286784537279901f7fac96518d5c75745645
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-18 18:40:03 +0100

    addded js file and logic

---
+++ app.js
@@ -0,0 +1,19 @@
+const shareButtons = document.querySelectorAll(".tile-share-button");
+
+async function copyText(e) {
+  // prevent button going to the site
+  e.preventDefault();
+  const link = this.getAttribute("link");
+  try {
+    await navigator.clipboard.writeText(link);
+    alert("Copied the text: " + link);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+shareButtons.forEach((shareButtons) =>
+  shareButtons.addEventListener("click", copyText)
+);
+
+
